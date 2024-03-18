e6d55d16937cdc599633e16094bbf5ace76140f9
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-03-18 17:07:25 +0300

    test fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,11 +8,11 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        if(clock < 0 || clock > 23) {
+        if (!isBarking || clock < 0 || clock > 23) {
             return false;
-        } else if(isBarking && (clock < 8 || clock > 22)) {
-            return  true;
-        }else {
+        } else if (clock < 8 || clock >= 20) {
+            return true;
+        } else {
             return false;
         }
 
@@ -21,36 +21,25 @@ public class Main {
 
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       if(firstAge > 13 && firstAge < 19 || secondAge > 13 && secondAge < 19 || thirdAge > 13 && thirdAge < 19) {
-           return  true;
-       } else {
-           return false;
-       }
+        return (firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-        int sicaklikLimit;
-        sicaklikLimit = isSummer ? 45 : 35;
-        if(temp >= 25 && temp <= sicaklikLimit) {
-            return true;
-        } else {
-            return false;
-        }
+        int upperLimit = (isSummer) ? 45 : 35;
+        return (temp >= 25 && temp <= upperLimit);
     }
 
     public static double area(double width, double height) {
-        if (width < 0 || height < 0) {
+        if (width < 0 || height < 0)
             return -1;
-        } else {
-            return  width * height;
-        }
+        return width * height;
     }
 
     public static double area(double radius) {
-        if(radius < 0) {
+        if (radius < 0)
             return -1;
-        } else {
-            return (radius * radius) * Math.PI;
-        }
+        return radius * radius * Math.PI;
     }
 }
