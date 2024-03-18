ccccbd2b963305ece0469def0342fdbf60ec36d4
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-03-18 15:19:10 +0300

    1.Soru

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,28 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if(clock < 0 || clock > 23) {
+            return false;
+        } else if(isBarking && (clock < 8 || clock > 22)) {
+            return  true;
+        }else {
+            return false;
+        }
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       return true;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        return true;
     }
 
     public static double area(double width, double height) {
-       
+        return double;
     }
 
     public static double area(double radius) {
-       
+        return double;
     }
 }
